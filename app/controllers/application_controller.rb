20256305879c2d1d5ce951efa1ecf28d0d25807a
Sarah <sarahlchavez@gmail.com>
2021-05-29 11:00:31 -0700

    commit tree c2deda786bf7ec58c2c053d59e1542ef90d0b624

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    # find user in database based on "username"
+    @user = User.find_by(username: params[:username])
 
+    # if there is a match, set the session to the user's ID
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+    # and redirect to /account route
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    #on the account page set up a link called 'Log Out' that clears the session
+    erb :account
   end
 
   get '/logout' do
-
+    # clear session to logout
+    session.clear
+    #redirect to home
+    redirect to '/'
   end
-
-
 end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+    def self.current_user(session_hash)
+        #use user_id from the session hash to find teh user in the database and return that user
+        User.find(session_hash[:user_id])
+    end
+
+    def self.is_logged_in?(session_hash)
+        !!session_hash[:user_id]
+
+    end
 end
